8b89edfe526b98d7e3908b16e7f7ecbb634cc0e8
Chrisgc06 <gomez.christophersteven@gmail.com>
2025-06-07 17:56:51 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using Mapa_conceptual;
+using System;
+using System.Collections.Generic;
+
+namespace Mapa_conceptual
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<MiembroDeLaComunidad> comunidad = new List<MiembroDeLaComunidad>
+            {
+                new Estudiante("Carlos"),
+                new ExAlumno("María"),
+                new Empleado("Luis"),
+                new Administrativo("Paula"),
+                new Docente("Pedro"),
+                new Maestro("Laura"),
+                new Administrador("José")
+            };
+
+            Console.WriteLine("=== Información de la Comunidad Educativa ===\n");
+
+            foreach (var persona in comunidad)
+            {
+                persona.MostrarInformacion();
+            }
+        }
+    }
+}
